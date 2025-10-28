96c3115b17b902f865c321f0e91bc9a4c163c3ae
neharika005 <91769477+neharika005@users.noreply.github.com>
2025-10-28 15:34:19 +0530

    Create AgentApprovalDto.java

---
+++ Real Estate Backend/real-estate/src/main/java/com/example/real_estate/dto/AgentApprovalDto.java
@@ -0,0 +1,17 @@
+package com.example.real_estate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AgentApprovalDto {
+    private Long id;
+    private String name;
+    private String email;
+    private boolean approved;
+}
