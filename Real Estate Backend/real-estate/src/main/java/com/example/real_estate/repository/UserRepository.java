d308c390f4558cb83323e71edf520e7183404ffe
neharika005 <91769477+neharika005@users.noreply.github.com>
2025-10-28 15:38:09 +0530

    Update UserRepository.java

--- Real Estate Backend/real-estate/src/main/java/com/example/real_estate/repository/UserRepository.java
+++ Real Estate Backend/real-estate/src/main/java/com/example/real_estate/repository/UserRepository.java
@@ -9,4 +9,6 @@ import com.example.real_estate.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+     List<User> findByRole_Name(String roleName);
 }
