04cc98e7f2e2dab6fa245649137eca794ceaa537
neharika005 <91769477+neharika005@users.noreply.github.com>
2025-10-28 05:30:09 +0530

    Update WebConfig.java

--- Real Estate Backend/real-estate/src/main/java/com/example/real_estate/config/WebConfig.java
+++ Real Estate Backend/real-estate/src/main/java/com/example/real_estate/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200","http://3.150.208.51:80")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
