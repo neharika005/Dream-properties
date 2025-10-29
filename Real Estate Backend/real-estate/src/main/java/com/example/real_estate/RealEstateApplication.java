7fb0c47347c88e79a705461b2213009014aa91f2
neharika005 <91769477+neharika005@users.noreply.github.com>
2025-10-29 07:02:07 +0530

    Update RealEstateApplication.java

--- Real Estate Backend/real-estate/src/main/java/com/example/real_estate/RealEstateApplication.java
+++ Real Estate Backend/real-estate/src/main/java/com/example/real_estate/RealEstateApplication.java
@@ -2,9 +2,10 @@ package com.example.real_estate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.example.real_estate")
+@ComponentScan(basePackages = "com.example.real_estate")
 public class RealEstateApplication {
     public static void main(String[] args) {
         SpringApplication.run(RealEstateApplication.class, args);
